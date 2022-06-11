8ab97e51aa3636245f50704553fd82b9f9706231
Halima-Ikbal <100046854+Halima-Ikbal@users.noreply.github.com>
2022-06-11 01:14:21 -0500

    added variable in script.js

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,2 +1,29 @@
 
-  
+  var searchform= $("#serch-form");
+  var history= $("#history");
+  var tooday= $("#tooday");
+  var future= $("#5-day-forecast");
+  var forecast= $("#forecast");
+
+  var cityList= []
+  var isClearing = false;
+  var clearMessageCode;
+
+  //Set the day
+  var today= dayjs();
+  function init() {
+    searchform.children("button").on("click", getData);
+    initiateStorage();
+    initiatePrev();
+  }
+
+
+
+
+
+
+
+
+
+
+  init();
