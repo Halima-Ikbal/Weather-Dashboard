2202a0e2de7bd8f55215f878395d9f9aa36fb465
Halima-Ikbal <100046854+Halima-Ikbal@users.noreply.github.com>
2022-06-11 02:00:57 -0500

    added api url

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -17,6 +17,42 @@
     initiatePrev();
   }
 
+  // checks local storage has any items and put in array
+  function initiateStorage() {
+    if(localStorage.getItem("cityList") !== null) {
+      cityList = JSON.parse(localStorage.getItem("cityList"));
+    }
+    localStorage.setItem("cityList", JSON.stringify(cityList));
+  }
+
+
+  var requestUrl =`https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=ed53a88702cbe5ce8a7ff26fd8f283d7`;
+  fetch(requestUrl)
+  // get response and turn it into objects
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      // get city's longitude and latitude
+      if(data.length) {
+        var lat=data[0].lat;
+        var lon=data[0].lon;
+        requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=ed53a88702cbe5ce8a7ff26fd8f283d7`
+        fetch(requestUrl)
+          .then(function (dresponse) {
+            return response.json();
+
+          })
+          .then(function (data){
+            displayWeather(data, city);
+            dispayForecast(data);
+            saveCity(city);
+          });
+      } else{
+        invalidInput();
+      }
+    });
+
 
 
 
